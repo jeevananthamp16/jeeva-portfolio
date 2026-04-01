577866e437f1d8211b7468dc4e9c33e11f6e26b1
jeevp <jeevp@cisco.com>
2026-04-01 22:41:39 +0530

    fix: force CSS cache bust for Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jeevananthamp16.github.io',
-  base: '/jeeva-portfolio',
+  site: 'https://portfolio.jeevanantham.co.in',
   devToolbar: {
     enabled: false
   },
